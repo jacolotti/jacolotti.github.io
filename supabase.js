b1e205271583c78bb0a9535535f26837bdd9e769
jacolotti <jacolotti@gmail.com>
2026-05-12 11:27:14 -0400

    Change supabaseClient assignment to window object

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = "https://bfolaqggvojflrfgqqga.supabase.co";
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmb2xhcWdndm9qZmxyZmdxcWdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MjQyMTYsImV4cCI6MjA5NDEwMDIxNn0.7HB2QWO6IRt-8QpclJMcbPf9rKWjsZW8xCodRzsxeDI";
 
-const supabaseClient = supabase.createClient(
+window.supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY,
   {
