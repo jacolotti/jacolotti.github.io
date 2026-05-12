7aa3fffe738debe24ccffd6ead21cb3e07d44e70
jacolotti <jacolotti@gmail.com>
2026-05-12 09:04:54 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,16 @@
 const SUPABASE_URL = "https://bfolaqggvojflrfgqqga.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_jJOC2o965W2neNAE4xVdtA_wsmt5joH";
+const SUPABASE_ANON_KEY = "YOUR_REAL_KEY_HERE";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+      storage: window.localStorage
+    }
+  }
 );
