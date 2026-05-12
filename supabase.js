1be99ad0517c860c5077c104dd2926a85f771b1a
jacolotti <jacolotti@gmail.com>
2026-05-12 09:11:11 -0400

    Update Supabase anon key in supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://bfolaqggvojflrfgqqga.supabase.co";
 
-const SUPABASE_ANON_KEY = "YOUR_REAL_KEY_HERE";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmb2xhcWdndm9qZmxyZmdxcWdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MjQyMTYsImV4cCI6MjA5NDEwMDIxNn0.7HB2QWO6IRt-8QpclJMcbPf9rKWjsZW8xCodRzsxeDI";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
